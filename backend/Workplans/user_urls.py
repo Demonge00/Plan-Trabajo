388f4243c83be5cad9901dca5652e91686db1956
Alejandro Figueredo <aflopez000430@gmail.com>
2024-05-07 21:47:21 -0400

    Feat: JWT

--- backend/Workplans/user_urls.py
+++ backend/Workplans/user_urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from Workplans import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('register/', views.registerUser, name='register_user'),
-    path('verify/<verification_secret>', views.verifyUser, name='verify-user')
+    path('verify/<verification_secret>', views.verifyUser, name='verify-user'),
+    path('token/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
